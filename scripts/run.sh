fadbc12a4a286f068a125247acb2e9c529fa3c97
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2025-05-01 15:51:11 -0400

    init

--- scripts/run.sh
+++
@@ -1,3 +0,0 @@
-# run.sh
-docker-compose -f docker-compose.dev.yml build --no-cache
-docker-compose -f docker-compose.dev.yml -p my-app up -d
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-// src/app/page.tsx
-import { redirect } from 'next/navigation';
-import { fallbackLng } from './i18n/settings';
-
-export default function Index() {
-  redirect(`/${fallbackLng}`);
-}
