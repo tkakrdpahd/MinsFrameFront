70224df4e4e06cf873854af4446f5bd28f8247be
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2025-05-18 13:23:49 -0400

    Header Update

--- src/fsd/widgets/header/index.tsx
+++ src/fsd/widgets/header/index.tsx
@@ -1,9 +1,26 @@
+import { NavigationMenu } from "radix-ui";
 import { Heading } from "@radix-ui/themes";
+import { getT } from "@/app/i18n";
 
-export default function Header() {
+export default async function Header() {
+  const { i18n, t } = await getT();
+
+  const currentLocale = i18n.language;
+
   return (
-    <header>
-      <Heading size="3">Header</Heading>
+    <header className="flex justify-between items-center p-4">
+      <Heading size="3">{t('header.title')}</Heading>
+      <NavigationMenu.Root>
+        <NavigationMenu.List>
+          <NavigationMenu.Item>
+            <NavigationMenu.Link href={`/${currentLocale}`}>{t('header.nav.home')}</NavigationMenu.Link>
+          </NavigationMenu.Item>
+
+          <NavigationMenu.Indicator />
+        </NavigationMenu.List>
+
+        <NavigationMenu.Viewport />
+      </NavigationMenu.Root>
     </header>
   );
 }
