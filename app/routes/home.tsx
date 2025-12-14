6e0424d3d8498e262f15ecaff3a420f86ba16f57
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2025-12-14 10:05:49 +0900

    init

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-import { MainPage } from "../pages/main/main";
+import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +9,5 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <MainPage />;
+  return <Welcome />;
 }
