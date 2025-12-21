646bfad13634e1f515b2f7ae885c617501cccf36
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2025-12-21 13:44:59 +0900

    Update pointer

--- src/features/pointer/model/particle.ts
+++ src/features/pointer/model/particle.ts
@@ -7,11 +7,11 @@ export const LIFE_DECREMENT = 1;
 
 export const GRAVITY = 0.1;
 
-export const PARTICLES_PER_FRAME = 10;
+export const PARTICLES_PER_FRAME = 8;
 export const PARTICLE_SPAWN_OFFSET_MIN = -0.25;
 export const PARTICLE_SPAWN_OFFSET_MAX = 0.25;
-export const PARTICLE_RADIUS_MIN = 1;
-export const PARTICLE_RADIUS_MAX = 3;
+export const PARTICLE_RADIUS_MIN = 3;
+export const PARTICLE_RADIUS_MAX = 5;
 
 export const INITIAL_VELOCITY_X_MIN = -0.5;
 export const INITIAL_VELOCITY_X_MAX = 0.5;
