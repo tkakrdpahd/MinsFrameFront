a7b37beb520ca027b0ef58e1f1603ebe2edf3982
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2025-12-24 10:24:36 +0900

    Update

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -12,6 +12,20 @@ import { Footer } from "~/widgets/footer/footer";
 
 import { Pointer } from "~/features/pointer/pointer";
 
+function Links() {
+    return (
+        <div className="flex flex-col items-center gap-4">
+            <span className="text-lg font-bold">Links</span>
+            <div className="flex flex-col md:flex-row items-center gap-4">
+                <Link to="https://github.com/tkakrdpahd" target="_blank" className="hover:underline">GitHub</Link>
+                <Link to="https://mdoo_pri.artstation.com" target="_blank" className="hover:underline">Artstation</Link>
+                <Link to="https://mdoo12.itch.io" target="_blank" className="hover:underline">itch.io</Link>
+                <Link to="https://scholar.google.com/citations?user=rfBJT_cAAAAJ&hl=en" target="_blank" className="hover:underline">Google Scholar</Link>
+            </div>
+        </div>
+    );
+}
+
 export function AboutPage() {
 
     return (
@@ -22,15 +36,9 @@ export function AboutPage() {
                 <Pointer />
             </div>
 
-            <main className="flex flex-col flex-1 items-center justify-center pt-16 pb-4 gap-4 relative z-10">
+            <main className="flex flex-col flex-1 pt-16 pb-4 gap-4 relative z-10">
                 <div className="flex flex-col items-center gap-4">
-                    <span className="text-lg font-bold">Links</span>
-                    <div className="flex flex-col md:flex-row items-center gap-4">
-                        <Link to="https://github.com/tkakrdpahd" target="_blank" className="hover:underline">GitHub</Link>
-                        <Link to="https://mdoo_pri.artstation.com" target="_blank" className="hover:underline">Artstation</Link>
-                        <Link to="https://mdoo12.itch.io" target="_blank" className="hover:underline">itch.io</Link>
-                        <Link to="https://scholar.google.com/citations?user=rfBJT_cAAAAJ&hl=en" target="_blank" className="hover:underline">Google Scholar</Link>
-                    </div>
+                    <Links />
                 </div>
             </main>
             
